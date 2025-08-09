675c89e620edc0eea0394ccd43c79de23731d581
Hexie <iagogarcia0197@gmail.com>
2025-08-08 23:00:41 -0300

    fix: replace nonexistent cloudtrail module with native aws_cloudtrail org trail

--- stacks/shared/cloudtrail-org/main.tf
+++ stacks/shared/cloudtrail-org/main.tf
@@ -12,29 +12,37 @@ provider "aws" {
   region = var.default_region
 }
 
+variable "default_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+# Inputs kept for symmetry with other stacks (security account may own the bucket/KMS)
 variable "security_account_id" {
   type    = string
   default = "000000000000"
 }
+
 variable "log_archive_bucket_name" {
   type    = string
   default = "org-cloudtrail-logs"
 }
 
-module "cloudtrail" {
-  source  = "terraform-aws-modules/cloudtrail/aws"
-  version = "~> 5.4"
-
+# Minimal org-wide CloudTrail (assumes bucket exists)
+resource "aws_cloudtrail" "org" {
   name                          = "org-trail"
   is_organization_trail         = true
-  enable_log_file_validation    = true
-  include_global_service_events = true
   is_multi_region_trail         = true
+  include_global_service_events = true
+  enable_log_file_validation    = true
 
   s3_bucket_name = var.log_archive_bucket_name
+
+  # If you later add a KMS key, uncomment:
+  # kms_key_id     = "arn:aws:kms:...:key/..."
+  # enable_log_file_validation = true
 }
 
-variable "default_region" {
-  type    = string
-  default = "us-east-1"
+output "trail_arn" {
+  value = aws_cloudtrail.org.arn
 }
